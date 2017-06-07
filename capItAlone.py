08a6c2ae31046a806a1466e63e31ccaa0710c48b
zhanxian1996 <zhanxian1996@gmail.com>
2017-06-07 15:22:34 -0400

    Add files via upload

---
+++ capItAlone.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+
+import Leap, sys, thread, time
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+starttime = time.time()
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+    def on_init(self, controller):
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+
+        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+              frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+
+        # Get hands
+        for hand in frame.hands:
+
+            handType = "Left hand" if hand.is_left else "Right hand"
+
+            print "  %s, id %d, position: %s" % (
+                handType, hand.id, hand.palm_position)
+
+            # Get the hand's normal vector and direction
+            normal = hand.palm_normal
+            direction = hand.direction
+
+            # Calculate the hand's pitch, roll, and yaw angles
+            print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
+                direction.pitch * Leap.RAD_TO_DEG,
+                normal.roll * Leap.RAD_TO_DEG,
+                direction.yaw * Leap.RAD_TO_DEG)
+
+            # Get arm bone
+            # arm = hand.arm
+            # print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
+            #     arm.direction,
+            #     arm.wrist_position,
+            #     arm.elbow_position)
+
+            # Get fingers
+            for finger in hand.fingers:
+
+                print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
+                    self.finger_names[finger.type],
+                    finger.id,
+                    finger.length,
+                    finger.width)
+
+        # Get tools
+        for tool in frame.tools:
+
+            print "  Tool id: %d, position: %s, direction: %s" % (
+                tool.id, tool.tip_position, tool.direction)
+
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+def main():
+	global starttime
+	# Create a sample listener and controller
+	listener = SampleListener()
+	controller = Leap.Controller()
+
+	# Have the sample listener receive events from the controller
+	controller.add_listener(listener)
+
+	# Keep this process running until Enter is pressed
+	if time.time()-starttime > 2:
+		# Remove the sample listener when done
+		controller.remove_listener(listener)
+
+if __name__ == "__main__":
+    main()
