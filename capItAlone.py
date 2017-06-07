e30c82fb46c73e4ca158fd2515ec7c0c755f92a5
kyletm <newportkyle@gmail.com>
2017-06-07 17:13:30 -0400

    Binary Password Detection

--- capItAlone.py
+++ capItAlone.py
@@ -2,6 +2,7 @@
 
 
 import Leap, sys, thread, time
+import matplotlib.pyplot as plt
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 starttime = time.time()
@@ -16,6 +17,7 @@ class SampleListener(Leap.Listener):
     ring = []
     pinky = []
     thumb = []
+    roll = []
     handlist = [thumb, index, mid, ring, pinky]
 
     def on_init(self, controller):
@@ -62,7 +64,7 @@ class SampleListener(Leap.Listener):
                 direction.pitch * Leap.RAD_TO_DEG,
                 normal.roll * Leap.RAD_TO_DEG,
                 direction.yaw * Leap.RAD_TO_DEG)
-
+            self.roll.append(normal.roll)
             # Get arm bone
             # arm = hand.arm
             # print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
@@ -72,7 +74,7 @@ class SampleListener(Leap.Listener):
 
             # Get fingers
             for finger in hand.fingers:
-            	self.handlist[finger.type].append(finger.direction)
+                self.handlist[finger.type].append(finger.direction)
 
                 # print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
                 #     self.finger_names[finger.type],
@@ -101,24 +103,68 @@ class SampleListener(Leap.Listener):
             return "STATE_INVALID"
 
 def main():
-	global starttime
-	# Create a sample listener and controller
-	listener = SampleListener()
-	controller = Leap.Controller()
-	
-	# Have the sample listener receive events from the controller
-	controller.add_listener(listener)
+    global starttime
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+    
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
 
-	# Keep this process running until 2 seconds have passed
-	while(time.time() - starttime < 2):
-		pass
-	else:
-		controller.remove_listener(listener)
-	for item in listener.handlist:
-		for items in item:
-			print type(items)
-	for item in listener.palm:
-		print type(item)
-	
+    # Keep this process running until 2 seconds have passed
+    while(time.time() - starttime < 2):
+        pass
+    else:
+        controller.remove_listener(listener)
+     # Compute average finger directional vectors
+    avgNorms = []
+    for vectorData in listener.handlist:
+         x = 0
+         y = 0
+         z = 0
+         numVecs = len(vectorData)
+         for vector in vectorData:
+             x += vector.x
+             y += vector.y
+             z += vector.z
+         avgNorms.append((x/numVecs,y/numVecs,z/numVecs))
+    # Compute average palm directional vector
+    x = 0
+    y = 0
+    z = 0
+    numVecs = len(vectorData)
+    for vector in listener.palm:
+          x += vector.x
+          y += vector.y
+          z += vector.z
+    avgPalm = (x/numVecs,y/numVecs,z/numVecs)
+    print('palm',avgPalm)
+    print([vector for vector in avgNorms])
+    # Compute absolute dot product of palm to finger directional vectors
+    dotProdSum = 0
+    for vector in avgNorms:
+        print(abs(sum(p*q for p,q in zip(avgPalm, vector))))
+        dotProdSum += abs(sum(p*q for p,q in zip(avgPalm, vector)))
+    if dotProdSum == 0:
+        print("Division by zero - hand might not have been detected")
+    else:
+        avgDot = dotProdSum/5
+        print('Average dot product',avgDot)
+    # Compute roll
+    avgRoll = sum([abs(num) for num in listener.roll])/len(listener.roll)
+    print('Average roll', avgRoll)
+    successful = check_tolerances(avgRoll,avgDot)
+    print("Password success: ", successful)
+    
+def check_tolerances(roll, dot):
+    # Check whether or not roll and dot match our binary password
+    # This position assumes a flat hand with fingers orthogonal to palm
+    ROLL_TOLERANCE = 0.2
+    DOT_TOLERANCE = 0.15
+    if roll <= ROLL_TOLERANCE and dot <= DOT_TOLERANCE:
+        return True
+    else:
+        return False
+    
 if __name__ == "__main__":
     main()
